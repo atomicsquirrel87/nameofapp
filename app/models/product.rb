fc8b04b27631303dfe28798507b38b8c0d6675a9
atomicsquirrel87 <domkes87@googlemail.com>
2018-03-12 15:51:41 +0100

    5.3 first step contact form without backend

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,9 @@
 class Product < ApplicationRecord
   has_many :orders
-  if Rails.env.production?
-    def self.search(search_term)
+  def self.search(search_term)
+    if Rails.env.production?
       Product.where("name ilike ?", "%#{search_term}%")
-    end
-  else Rails.env.development?
-    def self.search(search_term)
+    else Rails.env.development?
       Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'simple_pages/index'
 
+  post 'simple_pages/thank_you'
+
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
